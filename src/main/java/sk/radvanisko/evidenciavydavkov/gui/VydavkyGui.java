298d7ddbdf5f34c26106e9f1d02e54c8f048c047
radvanisko <radvanisko@users.noreply.github.com>
2023-03-17 21:09:35 +0100

    VydavkyGui sa stalo hlavne okno.
    novy vydavok -
    databaza aktualizovana
    //todo vyriesit aktualizaciu tabulky

--- src/main/java/sk/radvanisko/evidenciavydavkov/gui/VydavkyGui.java
+++ src/main/java/sk/radvanisko/evidenciavydavkov/gui/VydavkyGui.java
@@ -1,10 +1,18 @@
 package sk.radvanisko.evidenciavydavkov.gui;
 
+import sk.radvanisko.evidenciavydavkov.model.Sluzby;
+import sk.radvanisko.evidenciavydavkov.model.Vydavok;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class VydavkyGui {
 
@@ -19,18 +27,30 @@ public class VydavkyGui {
     private JLabel label;
     JPanel paneltable;
 
-    public JLabel getLabel() {
-        return label;
-    }
+// konstruktors
 
-    public void setLabel(JLabel label) {
-        this.label = label;
-    }
-
+    public VydavkyGui(JPanel panelHlavny) {  this.panelHlavny = panelHlavny; }
     public VydavkyGui(JTable table1) {
         this.table1 = table1;
     }
 
+
+
+    //gettery /settery
+    public JLabel getLabel() {
+        return label;
+    }
+    public void setLabel(JLabel label) {
+        this.label = label;
+    }
+
+    public JTable getTable1() {
+        return table1;
+    }
+
+    public void setTable1(JTable table1) {
+        this.table1 = table1;
+    }
 
     public VydavkyGui() {
 
@@ -38,7 +58,8 @@ public class VydavkyGui {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                ZadajNovy dialog = new ZadajNovy();
+
+                ZadajNovy dialog = new ZadajNovy(VydavkyGui.this);
                 dialog.pack();
                 dialog.setLocationRelativeTo(null); // vycentrovanie okna
                 dialog.setVisible(true);
@@ -50,13 +71,82 @@ public class VydavkyGui {
     }
 
 
-    //konstruktory
+    public static void main(String[] args) throws SQLException {
+
+        Sluzby sluzby = new Sluzby();
+        JTable table1 = new JTable();
+        VydavkyGui vydavkyGui = new VydavkyGui();
 
 
-    public VydavkyGui(JPanel panelHlavny) {
-        this.panelHlavny = panelHlavny;
+//      Object data[][] = null;
+        Object data[][] = {{"","", "", "", ""}, {"", "", "", ""}};
+        Object columnNames[] = {"Id","Popis vydavku", "Suma", "Datum ", "Kategoria"};
+
+        DefaultTableModel daDefaultTableModel = new DefaultTableModel(0, 0);
+        table1 = new JTable(data, columnNames);
+
+        JTableHeader header = table1.getTableHeader();
+
+        daDefaultTableModel.setColumnIdentifiers(columnNames);
+        table1.setModel(daDefaultTableModel);
+
+        vydavkyGui.paneltable.setLayout(new BorderLayout());
+        vydavkyGui.paneltable.add(header, BorderLayout.NORTH);
+        vydavkyGui.paneltable.add(table1, BorderLayout.CENTER);
+        vydavkyGui.paneltable.updateUI();
+
+
+// generovanie dummy riadkov
+
+/*
+        for (int i = 1; i <20; i++) {
+            daDefaultTableModel.addRow(new Object[] {"Popis"+(i),100-2*i,"15-3-2023","kategoria"});
+        }
+        table1.updateUI();
+*/
+
+
+        JFrame frame = new JFrame("Zoznam vydavkov");
+        frame.setContentPane(vydavkyGui.panelHlavny);
+        JScrollPane scrollPane = new JScrollPane(table1);
+
+        frame.setMinimumSize(new Dimension(700, 700));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null); // vycentrovanie okna
+        frame.pack();
+        frame.setVisible(true);
+
+
+//        scrollPane.setBounds(100, 40, 500, 500);
+        vydavkyGui.paneltable.add(scrollPane);
+
+
+        // vytvaranie prippojenia na Databazu
+
+        Connection conn = null;
+        String url = "jdbc:mysql://localhost:3306/vydavky";
+        String username = "root";
+        String password = "password";
+
+        if (conn == null) { // vytvorili sme tzv. singleton
+            conn = DriverManager.getConnection(url, username, password);
+            System.out.println("Databáza je pripojená!");
+            vydavkyGui.getLabel().setText("Databaza je pripojena");
+        }
+        //-----------------------------------------------------
+
+
+        ArrayList<Vydavok> vydavky = new ArrayList<Vydavok>();
+
+        vydavky = sluzby.vyberVsetkyMySql(conn);  //naplnil som arraylist vydavky z dtb
+
+
+
+        //naplnenie tabulky z arraylistu
+        for (Vydavok zoznam : vydavky) {
+            daDefaultTableModel.addRow(new Object[]{zoznam.getId(),zoznam.getPopisVydavku(), zoznam.getSuma(), zoznam.getDatum(), zoznam.getKategoria()});
+        }
+
+
     }
 }
-
-    //gettery /settery
-
--- src/main/java/sk/radvanisko/evidenciavydavkov/gui/ZadajNovy.java
+++ src/main/java/sk/radvanisko/evidenciavydavkov/gui/ZadajNovy.java
@@ -1,7 +1,15 @@
 package sk.radvanisko.evidenciavydavkov.gui;
 
+import sk.radvanisko.evidenciavydavkov.model.Sluzby;
+import sk.radvanisko.evidenciavydavkov.model.Vydavok;
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.*;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ZadajNovy extends JDialog {
     private JPanel contentPane;
@@ -12,14 +20,22 @@ public class ZadajNovy extends JDialog {
     private JTextField textField3;
     private JTextField textField4;
 
-    public ZadajNovy() {
+    private final VydavkyGui vydavkyGui;
+
+    public ZadajNovy(VydavkyGui vydavkyGui) {
+
+        this.vydavkyGui = vydavkyGui;
         setContentPane(contentPane);
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
 
         buttonOK.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                onOK();
+                try {
+                    onOK();
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
             }
         });
 
@@ -43,11 +59,56 @@ public class ZadajNovy extends JDialog {
                 onCancel();
             }
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+
     }
 
-    private void onOK() {
+    private void onOK() throws SQLException {
         // add your code here
         System.out.println("bol stlačeny OK");
+
+        Sluzby sluzby=new Sluzby();
+
+        // vlozenie do objektu novy vydavok
+        Vydavok novyvydavok=new Vydavok();
+
+
+            novyvydavok.setPopisVydavku(textField1.getText());
+
+            novyvydavok.setSuma(Double.parseDouble(textField2.getText())); //treba osetrit
+
+//        novyvydavok.setDatum(textField3.getText());
+        novyvydavok.setKategoria(textField4.getText());
+
+
+        // vytvaranie prippojenia na Databazu
+
+        Connection conn = null;
+        String url = "jdbc:mysql://localhost:3306/vydavky";
+        String username = "root";
+        String password = "password";
+
+        if (conn == null) { // vytvorili sme tzv. singleton
+            conn = DriverManager.getConnection(url, username, password);
+            System.out.println("Databáza je pripojená!");
+            vydavkyGui.getLabel().setText("Databaza je pripojena");
+        }
+        //-----------------------------------------------------
+
+
+        sluzby.vlozVydavokMySql(conn,novyvydavok);  //vloz do databazy conn
+
+        JTable table1 = vydavkyGui.getTable1();
+
+        // novyvydavok.getId() toto asi nie v objekte
+
+//todo aktualiyovat tabulku
+        System.out.println(novyvydavok.getId()+novyvydavok.getPopisVydavku()+novyvydavok.getSuma()+novyvydavok.getKategoria());
+
+        Object[] data = {novyvydavok.getId(),novyvydavok.getPopisVydavku(), novyvydavok.getSuma(), novyvydavok.getDatum(), novyvydavok.getKategoria()};
+        DefaultTableModel model = (DefaultTableModel) table1.getModel();
+        model.addRow(data);
+        table1.updateUI();
+
         dispose();
     }
 
@@ -57,10 +118,4 @@ public class ZadajNovy extends JDialog {
         dispose();
     }
 
-/*    public static void main(String[] args) {
-        ZadajNovy dialog = new ZadajNovy();
-        dialog.pack();
-        dialog.setVisible(true);
-        System.exit(0);
-    }*/
 }
